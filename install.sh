b5139957356bc9cc7ef63c965a787346c9a3a9ee
Wes Hedrick <wes.hedrick@vanta.com>
2026-03-19 09:15:10 -0400

    add some basic dot files

---
+++ install.sh
@@ -0,0 +1,29 @@
+ #!/bin/bash  
+
+ # Needed to default to zsh shell when SSH'ing in
+ sudo chsh "$(id -un)" --shell "/usr/bin/zsh"
+
+ # Install fd and ripgrep
+ sudo apt-get update -qq
+ sudo apt-get install -y fd-find ripgrep
+
+   # fd installs as fdfind on Debian/Ubuntu, symlink to fd
+   sudo ln -sf "$(which fdfind)" /usr/local/bin/fd
+  
+   create_symlinks() {
+       # Get the directory in which this script lives.
+       script_dir=$(dirname "$(readlink -f "$0")")
+  
+       # Get a list of all files in this directory that start with a dot.
+       files=$(find -maxdepth 1 -type f -name ".*")
+  
+       # Create a symbolic link to each file in the home directory.
+       for file in $files; do
+           name=$(basename $file)
+           echo "Creating symlink to $name in home directory."
+           rm -rf ~/$name
+           ln -s $script_dir/$name ~/$name
+       done
+   }
+  
+   create_symlinks
