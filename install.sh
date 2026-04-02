ea14bc7702961c6e57530e9557d9680032fe01bf
Wes Hedrick <wes.hedrick@vanta.com>
2026-04-02 09:55:52 -0400

    add rtk

--- install.sh
+++ install.sh
@@ -1,4 +1,4 @@
- #!/bin/bash  
+ #!/bin/bash
 
  # Needed to default to zsh shell when SSH'ing in
  sudo chsh "$(id -un)" --shell "/usr/bin/zsh"
@@ -6,17 +6,19 @@
  # Install fd and ripgrep
  sudo apt-get update -qq
  sudo apt-get install -y fd-find ripgrep
+ curl -fsSL https://raw.githubusercontent.com/rtk-ai/rtk/refs/heads/master/install.sh | sh
+ rtk init --global
 
    # fd installs as fdfind on Debian/Ubuntu, symlink to fd
    sudo ln -sf "$(which fdfind)" /usr/local/bin/fd
-  
+
    create_symlinks() {
        # Get the directory in which this script lives.
        script_dir=$(dirname "$(readlink -f "$0")")
-  
+
        # Get a list of all files in this directory that start with a dot.
        files=$(find -maxdepth 1 -type f -name ".*")
-  
+
        # Create a symbolic link to each file in the home directory.
        for file in $files; do
            name=$(basename $file)
@@ -25,5 +27,5 @@
            ln -s $script_dir/$name ~/$name
        done
    }
-  
+
    create_symlinks
